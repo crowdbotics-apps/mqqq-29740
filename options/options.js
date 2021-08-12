e9aa80be57508b117dd734bcf35230d3a2a42d77
Crowdbotics <team@crowdbotics.com>
2021-08-12 01:22:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mqqq_29740Identifier",
+  url: "https://mqqq_29740Identifier.botics.co",
+  api: "https://mqqq_29740Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
